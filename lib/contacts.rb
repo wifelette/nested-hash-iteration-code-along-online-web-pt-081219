3d9c7ed5acf866c4bcb254ce6cc583ad4c562718
Leah Silber <leahsilber@gmail.com>
2019-08-29 22:21:43 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,7 @@ require 'pry'
   # }
 
   def remove_strawberry(contacts)
-    contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete("strawberry") 
+    contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete("strawberry")
     contacts
   end
 
