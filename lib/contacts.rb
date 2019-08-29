ef5439af244d9ba71f22d38d0144efb328e5de86
Leah Silber <leahsilber@gmail.com>
2019-08-29 22:09:23 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,13 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
-def remove_strawberry(contacts)
 
+def remove_strawberry(contacts)
+  contacts.each do |person_k, info_v|
+    person_flavors = person_k[favorite_ice_cream_flavors]
+
+    if person_flavors.include("strawberry")
+      person_flavors.delete_if { |a| a == "strawberry" }
+    end
+  end
 end
